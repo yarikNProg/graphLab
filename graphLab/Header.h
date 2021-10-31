a9eef761f47cf626a27c3de819089c04457255ab
yaroslavN6 <yariknozdrin@yandex.ru>
2021-10-31 19:34:35 +0500

    Добавьте файлы проекта.

---
+++ graphLab/Header.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <ostream>
+#include <istream>
+#include <fstream>
+#include <iomanip>
+
+#include <algorithm>
+#include <cmath>
+
+#include <ctime>
+
+#include <vector>
+#include <string>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+#include <Windows.h>
+
+#include <SDL.h>
+
+#include <glad/glad.h>
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+using namespace glm;
+
+#pragma comment( lib, "SDL2.lib")
+#pragma comment( lib, "SDL2main.lib")
+#pragma comment( lib, "OpenGL32.lib")
